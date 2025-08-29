2673381afb095a12db0cdbd6dc7467f563d608c3
ClaytonDewey <clay@dryadmedia.com>
2025-08-29 15:46:22 -0500

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import useLocalStorage from 'use-local-storage';
+import './App.scoped.css';
+import Square from './components/Square';
 
 function App() {
+  const defaultDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+  const [theme, setTheme] = useLocalStorage(
+    'theme',
+    defaultDark ? 'dark' : 'light'
+  );
+
+  const switchTheme = () => {
+    const newTheme = theme === 'light' ? 'dark' : 'light';
+    setTheme(newTheme);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app' data-theme={theme}>
+      <span>Easy Darkmode and Themes in React</span>
+      <button onClick={switchTheme}>
+        Switch to {theme === 'light' ? 'Dark' : 'Light'} Theme
+      </button>
+      <Square />
     </div>
   );
 }
---
+++ src/components/Square.tsx
@@ -0,0 +1,10 @@
+import './square.scoped.css';
+
+const Square = () => {
+  return (
+    <div className='square'>
+      <span id='square-text'>Look Ma, I'm a Square!</span>
+    </div>
+  );
+};
+export default Square;
